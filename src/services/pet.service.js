9df64de79ad7f82b5288988c812380eb2f73419f
wellesleymussolini <iei.muss@gmail.com>
2023-04-02 08:02:13 -0300

    pet services

---
+++ src/services/pet.service.js
@@ -0,0 +1,11 @@
+import { Pet } from "../models/pet.model.js";
+import mongoose from "mongoose";
+
+export const PetService = {
+    async findByParam(param){
+        let find_pet;
+        if(mongoose){
+
+        }
+    }
+};
