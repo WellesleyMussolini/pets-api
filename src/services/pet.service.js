a1d333f4fb25319d31eec9fd19f3c72f719eb7e0
wellesleymussolini <iei.muss@gmail.com>
2023-06-05 19:58:35 -0300

    find pet by: 'language' & 'name and Id'

--- src/services/pet.service.js
+++ src/services/pet.service.js
@@ -3,16 +3,44 @@ import mongoose from "mongoose";
 
 export const PetService = {
     async findByParam(param) {
-        let find_pet;
-        if (mongoose.Types.ObjectId.isValid(param)) {
-            find_pet = await Pet.findById(param); // if the param is a valid id, it will search by id
-        } else {
-            // if isn't a valid id, it'll assume that is a breed
-            const breed = param.replace(/-/g, " ");
-            const lowercase_breed_search = new RegExp(breed, "i");
-            find_pet = await Pet.findOne({ breed: lowercase_breed_search });
-            return find_pet;
+        try {
+            if (mongoose.Types.ObjectId.isValid(param)) {
+                return await Pet.findById(param); // if the param is a valid id, it will search by id
+            } else {
+                // if it isn't a valid id, it'll assume that it is a breed
+                const breed = param.replace(/-/g, " ");
+                const lowercase_breed_search = new RegExp(breed, "i");
+                return await Pet.findOne({ breed: lowercase_breed_search });
+            }
+        } catch {
+            throw new Error("Error finding pet");
         }
-        return find_pet; 
-    }
+    },
+    async findByLang(param, language) {
+        const pet = await this.findByParam(param);
+
+        const languages = {
+            en: "en",
+            ptBr: "ptBr",
+        };
+
+        if (languages.hasOwnProperty(language)) {
+            const langKey = languages[language];
+            return {
+                breed: pet.breed,
+                life_span: pet.life_span[langKey],
+                breed_traits: pet.breed_traits[langKey],
+                description: pet.description[langKey],
+                temperament: pet.temperament[langKey],
+                intelligence: pet.intelligence[langKey],
+                breed_group: pet.breed_group[langKey],
+                breed_size: pet.breed_size[langKey],
+                weight: pet.weight,
+                images: pet.images,
+                breed_characteristics: pet.breed_characteristics[langKey],
+            };
+        } else {
+            throw new Error("Language not supported");
+        }
+    },
 };
