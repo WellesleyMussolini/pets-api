042972996b67e67037a3ab20d2f54826336f4b19
WellesleyMussolini <iei.muss@gmail.com>
2023-07-17 20:25:49 -0300

    Refactored services to improve breed search functionality and language support

--- src/services/pet.service.js
+++ src/services/pet.service.js
@@ -1,18 +1,24 @@
-import { Pet } from "../models/pet.model.js";
+import { Pet } from "../schemas/pet.schema.js";
 import mongoose from "mongoose";
+import { removeSpecialChars } from "../utils/removeSpecialChars.js";
 
 export const PetService = {
     async findByParam(param) {
         try {
-            if (mongoose.Types.ObjectId.isValid(param)) {
-                return await Pet.findById(param); // if the param is a valid id, it will search by id
-            } else {
-                // if it isn't a valid id, it'll assume that it is a breed
-                const breed = param.replace(/-/g, " ");
-                const lowercase_breed_search = new RegExp(breed, "i");
-                return await Pet.findOne({ breed: lowercase_breed_search });
-            }
-        } catch {
+            if (mongoose.Types.ObjectId.isValid(param)) return await Pet.findById(param); // If the param is a valid ID, it will search by ID
+
+            // If it isn't a valid ID, it'll assume that it is a breed
+            const normalizedBreed = removeSpecialChars(param).replace(/-/g, " ").toLowerCase();
+            const regex = new RegExp(`^${normalizedBreed}$`, "i");
+
+            return await Pet.findOne({
+                $or: [
+                    { breed: regex },
+                    { "breed.en": regex },
+                    { "breed.ptBr": regex },
+                ],
+            });
+        } catch (error) {
             throw new Error("Error finding pet");
         }
     },
@@ -24,23 +30,26 @@ export const PetService = {
             ptBr: "ptBr",
         };
 
-        if (languages.hasOwnProperty(language)) {
-            const langKey = languages[language];
-            return {
-                breed: pet.breed,
-                life_span: pet.life_span[langKey],
-                breed_traits: pet.breed_traits[langKey],
-                description: pet.description[langKey],
-                temperament: pet.temperament[langKey],
-                intelligence: pet.intelligence[langKey],
-                breed_group: pet.breed_group[langKey],
-                breed_size: pet.breed_size[langKey],
-                weight: pet.weight,
-                images: pet.images,
-                breed_characteristics: pet.breed_characteristics[langKey],
-            };
-        } else {
+        if (!languages.hasOwnProperty(language)) {
             throw new Error("Language not supported");
         }
+
+        const langKey = languages[language];
+        const breed = pet.breed;
+        const breedText = typeof breed === "string" ? breed : breed[langKey];
+
+        return {
+            breed: breedText,
+            life_span: pet.life_span[langKey],
+            breed_traits: pet.breed_traits[langKey],
+            description: pet.description[langKey],
+            temperament: pet.temperament[langKey],
+            intelligence: pet.intelligence[langKey],
+            breed_group: pet.breed_group[langKey],
+            breed_size: pet.breed_size[langKey],
+            weight: pet.weight,
+            images: pet.images,
+            breed_characteristics: pet.breed_characteristics[langKey],
+        };
     },
 };
